a01bd175f411b10752c3fd58ddb115b7adcbcba8
James David <james.designer19@gmail.com>
2024-12-03 21:23:02 -0800

    Media query update

---
+++ script.js
@@ -0,0 +1,20 @@
+/*function colapseSideBar(){
+    const side_nav_container_colapse = document.querySelector(`.side_nav_container_colapse`)
+    .side_nav_container_colapse.style.display = `flex`
+}
+
+alert(`youtube`)
+
+
+
+/*<script>
+    function showSidenav(){
+        const side_nav_mobile = document.querySelector('.side_nav_mobile')
+        side_nav_mobile.style.display = 'flex'
+    }
+
+    function hideSidenav(){
+        const side_nav_mobile = document.querySelector('.side_nav_mobile')
+        side_nav_mobile.style.display = 'none'
+    }
+</script>*/
